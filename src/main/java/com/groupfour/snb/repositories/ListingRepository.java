61bcc74101ba677d9ebdf07c8efaf0faa7854da0
franklinnevesfilho <franklin.neves.filho@gmail.com>
2023-09-01 13:23:18 -0400

    added Listings Crud

---
+++ src/main/java/com/groupfour/snb/repositories/ListingRepository.java
@@ -0,0 +1,18 @@
+
+package com.groupfour.snb.repositories;
+
+import com.groupfour.snb.models.listing.Listing;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/*
+
+ CRUD stands for:
+    C - create
+    R - read
+    U - update
+    D - delete
+
+ */
+@Repository
+public interface ListingRepository extends CrudRepository<Listing,Integer> {}
